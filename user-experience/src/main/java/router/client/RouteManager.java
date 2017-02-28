48f6a96dc4aa92cff245a953ed19193f099703d8
Peter Donald <peter@realityforge.org>
2017-02-28 21:08:57 +1100

    Extract out routerManager

---
+++ user-experience/src/main/java/router/client/RouteManager.java
@@ -0,0 +1,128 @@
+package router.client;
+
+import elemental2.Element;
+import elemental2.Global;
+import elemental2.Window;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Stream;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+@SuppressWarnings( { "ConstantConditions", "WeakerAccess" } )
+public final class RouteManager
+{
+  @Nonnull
+  private final RouteDefinition[] _routes;
+  @Nonnull
+  private final Element _rootElement;
+  @Nullable
+  private String _defaultHash;
+  private Object _callback;
+
+  public RouteManager( @Nonnull final RouteDefinition[] routes,
+                       @Nonnull final Element rootElement,
+                       @Nullable final String defaultHash )
+  {
+    assert null != routes;
+    assert null != rootElement;
+    assert Stream.of( routes ).noneMatch( Objects::isNull );
+
+    _routes = routes;
+    _rootElement = rootElement;
+    _defaultHash = defaultHash;
+  }
+
+  public void install()
+  {
+    uninstall();
+    final Window.AddEventListenerListenerCallback callback = ( e ) -> onHashChange();
+    Global.window.addEventListener( "hashchange", callback, false );
+    _callback = callback;
+  }
+
+  public void uninstall()
+  {
+    if( null != _callback )
+    {
+      Global.window.removeEventListener( "hashchange", (Window.RemoveEventListenerListenerCallback) _callback, false );
+      _callback = null;
+    }
+  }
+
+  private boolean onHashChange()
+  {
+    route();
+    return true;
+  }
+
+  @Nullable
+  public Route route()
+  {
+    final String hash = JsObjects.get( Global.window.location, "hash" );
+
+    final Route route = attemptRouteMatch( hash );
+    if ( null != route )
+    {
+      return route;
+    }
+    else if ( null != _defaultHash )
+    {
+      JsObjects.set( Global.window.location, "hash", _defaultHash );
+      return attemptRouteMatch( hash );
+    }
+    else
+    {
+      return null;
+    }
+  }
+
+  private Route attemptRouteMatch( final String hash )
+  {
+    for ( final RouteDefinition definition : _routes )
+    {
+      final Map<String, Object> routeData = definition.match( hash );
+      final Route route = new Route( definition, routeData );
+      if ( null != routeData && processRoute( route ) )
+      {
+        return route;
+      }
+    }
+    return null;
+  }
+
+  private boolean processRoute( @Nonnull final Route route )
+  {
+    assert null != route;
+    final RouteDefinition definition = route.getDefinition();
+    final PreRouteGuardCallback preRouteGuard = definition.getPreRouteGuard();
+    if ( preRouteGuard == null || preRouteGuard.preRouteGuard( route ) )
+    {
+      final BeforeRouteCallback beforeRoute = definition.getBeforeRoute();
+      if ( null != beforeRoute )
+      {
+        beforeRoute.beforeRoute( route, () -> completeRouting( route ) );
+      }
+      else
+      {
+        completeRouting( route );
+      }
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+
+  private void completeRouting( @Nonnull final Route route )
+  {
+    final RouteDefinition definition = route.getDefinition();
+    definition.getRoute().route( route, _rootElement );
+    final PostRouteCallback postRoute = definition.getPostRoute();
+    if ( null != postRoute )
+    {
+      postRoute.postRoute( route );
+    }
+  }
+}
--- user-experience/src/main/java/router/client/Router.java
+++ user-experience/src/main/java/router/client/Router.java
@@ -2,120 +2,73 @@ package router.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import elemental2.Element;
-import elemental2.EventTarget;
 import elemental2.Global;
-import elemental2.HTMLDivElement;
-import elemental2.HashChangeEvent;
-import elemental2.Node;
 import elemental2.RegExp;
-import java.util.Map;
-import javax.annotation.Nonnull;
+import java.util.Objects;
 
 public final class Router
   implements EntryPoint
 {
-  private final RouteDefinition[] _routes = new RouteDefinition[]
-    {
-      new RouteDefinition( "#/", null, null, ( route, element ) -> route1( element ), null ),
-      new RouteDefinition( "#/foo", null, null, ( route, element ) -> route2( element, "foo" ), null ),
-      new RouteDefinition( new RegExp( "^#/baz/(\\d+)/(\\d)$", "" ),
-                           new String[]{ "bazID", "buzID" },
-                           null,
-                           null,
-                           ( route, element ) -> route2( element, "baz2" ),
-                           null ),
-      new RouteDefinition( new RegExp( "^#/baz/(\\d+)$", "" ),
-                           new String[]{ "bazID" },
-                           ( route -> route.getRouteData().get( "bazID" ).equals( "42" ) ),
-                           null,
-                           ( route, element ) -> route2( element, "baz" ),
-                           null ),
-      new RouteDefinition( new RegExp( "^#/biz/(\\d+)$", "" ),
-                           new String[]{ "bazID" },
-                           ( route -> route.getRouteData().get( "bazID" ).equals( "42" ) ),
-                           null,
-                           ( route, element ) -> route2( element, "biz" ),
-                           ( route -> info( "PostRoute " + route ) ) ),
-      };
+  private RouteManager _routeManager;
 
-  private Node route1( final Element element )
+  private void route2( final Element element, final String string )
   {
-    final HTMLDivElement div = (HTMLDivElement) Global.document.createElement( "div" );
-    div.innerHTML = "<h1>route1</h1>";
-    return element.appendChild( div );
-  }
-
-  private Node route2( final Element element, final String string )
-  {
-    final HTMLDivElement div = (HTMLDivElement) Global.document.createElement( "div" );
-    div.innerHTML = "<h1>" + string + "</h1>";
-    return element.appendChild( div );
+    element.innerHTML = "<h1>" + string + "</h1>";
   }
 
   public void onModuleLoad()
   {
-    final EventTarget.AddEventListenerListenerCallback callback = ( a ) -> route( (HashChangeEvent) a );
-    Global.window.addEventListener( "hashchange", callback, false );
+    final Element rootElement = Global.document.getElementById( "hook" );
+
+    final RouteDefinition[] routes = new RouteDefinition[]
+      {
+        new RouteDefinition( "#/", null, null, ( route, element ) -> route2( element, "#/" ), null ),
+        new RouteDefinition( "#/foo", null, null, ( route, element ) -> route2( element, "#/foo" ), null ),
+        new RouteDefinition( new RegExp( "^#/baz/(\\d+)/(\\d+)$", "" ),
+                             new String[]{ "bazID", "buzID" },
+                             null,
+                             null,
+                             ( route, element ) -> route2( element,
+                                                           "#/baz/" +
+                                                           route.getData( "bazID" ) +
+                                                           "/" +
+                                                           route.getData( "buzID" ) ),
+                             null ),
+        new RouteDefinition( new RegExp( "^#/baz/(\\d+)$", "" ),
+                             new String[]{ "bazID" },
+                             ( route -> Objects.equals( route.getData( "bazID" ), "42" ) ),
+                             null,
+                             ( route, element ) -> route2( element, "#/baz/" + route.getData( "bazID" ) ),
+                             null ),
+        new RouteDefinition( new RegExp( "^#/biz/(\\d+)$", "" ),
+                             new String[]{ "bazID" },
+                             ( route -> Objects.equals( route.getData( "bazID" ), "42" ) ),
+                             null,
+                             ( route, element ) -> route2( element, "#/biz/" + route.getData( "bazID" ) ),
+                             ( route -> info( "PostRoute " + route ) ) ),
+        new RouteDefinition( new RegExp( "^#/end$", "" ),
+                             new String[ 0 ],
+                             null,
+                             null,
+                             ( route, element ) ->
+                             {
+                               route2( element, "#/end" );
+                               _routeManager.uninstall();
+                             },
+                             null ),
+        };
+
+    _routeManager = new RouteManager( routes, rootElement, "#/" );
+    _routeManager.install();
     try
     {
       info( "Router started" );
-      route();
+      _routeManager.route();
     }
     catch ( final Exception e )
     {
       warn( "Unexpected problem initializing the Router application: " + e );
       Global.window.alert( "Error: " + e.getMessage() );
-    }
-  }
-
-  private boolean route( @Nonnull final HashChangeEvent e )
-  {
-    info( "event.newURL: " + e.newURL );
-    info( "event.oldURL: " + e.oldURL );
-    route();
-    return true;
-  }
-
-  private void route()
-  {
-    final String hash = JsObjects.get( Global.window.location, "hash" );
-    info( "location.hash: " + hash );
-
-    final Element rootElement = Global.document.getElementById( "hook" );
-    for ( final RouteDefinition definition : _routes )
-    {
-      final Map<String, Object> routeData = definition.match( hash );
-      if ( null != routeData )
-      {
-        final Route route = new Route( definition, routeData );
-        final PreRouteGuardCallback preRouteGuard = definition.getPreRouteGuard();
-        if ( preRouteGuard == null || preRouteGuard.preRouteGuard( route ) )
-        {
-          final BeforeRouteCallback beforeRoute = definition.getBeforeRoute();
-          if ( null != beforeRoute )
-          {
-            beforeRoute.beforeRoute( route, () -> performRoute( route, rootElement ) );
-          }
-          else
-          {
-            performRoute( route, rootElement );
-          }
-          return;
-        }
-      }
-    }
-
-    JsObjects.set( Global.window.location, "hash", "#/" );
-  }
-
-  private void performRoute( @Nonnull final Route route, @Nonnull final Element rootElement )
-  {
-    final RouteDefinition definition = route.getDefinition();
-    definition.getRoute().route( route, rootElement );
-    final PostRouteCallback postRoute = definition.getPostRoute();
-    if ( null != postRoute )
-    {
-      postRoute.postRoute( route );
     }
   }
 
