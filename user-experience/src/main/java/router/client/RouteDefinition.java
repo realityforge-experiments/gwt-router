20cd57d94bde63eaee23af230b0e5c0e953398dc
Peter Donald <peter@realityforge.org>
2017-03-28 09:34:11 +1100

    Simplify code

--- user-experience/src/main/java/router/client/RouteDefinition.java
+++ user-experience/src/main/java/router/client/RouteDefinition.java
@@ -2,56 +2,35 @@ package router.client;
 
 import java.util.HashMap;
 import java.util.Map;
-import javax.annotation.Nonnegative;
+import java.util.Objects;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 public final class RouteDefinition
 {
-  //The relative priority of the route. Lower priorities are higher priority. Priority should not be below zero.
-  @Nonnegative
-  private final int _priority;
   @Nonnull
   private final RegExp _matcher;
   @Nullable
   private final String[] _parameterKeys;
-  @Nonnull
-  private final String _path;
   @Nullable
   private final PreRouteGuardCallback _preRouteGuard;
   @Nullable
   private final BeforeRouteCallback _beforeRoute;
   @Nonnull
   private final RouteCallback _route;
-  @Nonnull
+  @Nullable
   private final UpdateRouteCallback _updateRoute;
   @Nullable
   private final PostRouteCallback _postRoute;
-  /**
-   * Controls whether this onEnter definition finalizes the search for a onEnter or just adds callbacks to the chain.
-   */
-  private final boolean _terminal;
 
-  @SuppressWarnings( "ConstantConditions" )
-  public RouteDefinition( @Nonnegative final int priority,
-                          @Nonnull final String path,
+  public RouteDefinition( @Nonnull final String path,
                           @Nullable final PreRouteGuardCallback preRouteGuard,
                           @Nullable final BeforeRouteCallback beforeRoute,
                           @Nonnull final RouteCallback route,
                           @Nullable final UpdateRouteCallback updateRoute,
-                          @Nullable final PostRouteCallback postRoute,
-                          final boolean terminal )
+                          @Nullable final PostRouteCallback postRoute )
   {
-    this( priority,
-          path,
-          pathToRegex( path ),
-          new String[ 0 ],
-          preRouteGuard,
-          beforeRoute,
-          route,
-          updateRoute,
-          postRoute,
-          terminal );
+    this( pathToRegex( path ), new String[ 0 ], preRouteGuard, beforeRoute, route, updateRoute, postRoute );
   }
 
   static RegExp pathToRegex( @Nonnull final String path )
@@ -59,68 +38,21 @@ public final class RouteDefinition
     return new RegExp( "^" + path.replaceAll( "([\\-/\\\\\\^$\\*\\+\\?\\.\\(\\)\\|\\[\\]\\{\\}])", "\\\\$1" ) + "$" );
   }
 
-  @SuppressWarnings( "ConstantConditions" )
-  public RouteDefinition( @Nonnegative final int priority,
-                          @Nonnull final RegExp matcher,
+  public RouteDefinition( @Nonnull final RegExp matcher,
                           @Nonnull final String[] parameterKeys,
                           @Nullable final PreRouteGuardCallback preRouteGuard,
                           @Nullable final BeforeRouteCallback beforeRoute,
                           @Nonnull final RouteCallback route,
                           @Nullable final UpdateRouteCallback updateRoute,
-                          @Nullable final PostRouteCallback postRoute,
-                          final boolean terminal )
+                          @Nullable final PostRouteCallback postRoute )
   {
-    this( priority,
-          matcher.toString(),
-          matcher,
-          parameterKeys,
-          preRouteGuard,
-          beforeRoute,
-          route,
-          updateRoute,
-          postRoute,
-          terminal );
-  }
-
-  @SuppressWarnings( "ConstantConditions" )
-  private RouteDefinition( @Nonnegative final int priority,
-                           @Nonnull final String path,
-                           @Nonnull final RegExp matcher,
-                           @Nonnull final String[] parameterKeys,
-                           @Nullable final PreRouteGuardCallback preRouteGuard,
-                           @Nullable final BeforeRouteCallback beforeRoute,
-                           @Nonnull final RouteCallback route,
-                           @Nullable final UpdateRouteCallback updateRoute,
-                           @Nullable final PostRouteCallback postRoute,
-                           final boolean terminal )
-  {
-    assert priority >= 0;
-    assert null != path;
-    assert null != matcher;
-    assert null != parameterKeys;
-    assert null != route;
-    _priority = priority;
-    _path = path;
-    _parameterKeys = parameterKeys;
-    _matcher = matcher;
+    _parameterKeys = Objects.requireNonNull( parameterKeys );
+    _matcher = Objects.requireNonNull( matcher );
     _preRouteGuard = preRouteGuard;
     _beforeRoute = beforeRoute;
-    _route = route;
+    _route = Objects.requireNonNull( route );
     _updateRoute = updateRoute;
     _postRoute = postRoute;
-    _terminal = terminal;
-  }
-
-  @Nonnegative
-  public int getPriority()
-  {
-    return _priority;
-  }
-
-  @Nonnull
-  public String getPath()
-  {
-    return _path;
   }
 
   @Nullable
@@ -141,7 +73,7 @@ public final class RouteDefinition
     return _route;
   }
 
-  @Nonnull
+  @Nullable
   public UpdateRouteCallback getUpdateRoute()
   {
     return _updateRoute;
@@ -151,11 +83,6 @@ public final class RouteDefinition
   public PostRouteCallback getPostRoute()
   {
     return _postRoute;
-  }
-
-  public boolean isTerminal()
-  {
-    return _terminal;
   }
 
   @SuppressWarnings( "ConstantConditions" )
