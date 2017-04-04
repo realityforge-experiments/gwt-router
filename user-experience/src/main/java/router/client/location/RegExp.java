031133d7cb51a93183c191cba099f9ac165179a6
Peter Donald <peter@realityforge.org>
2017-04-04 18:03:42 +1000

    Limit scope of suppression

--- user-experience/src/main/java/router/client/location/RegExp.java
+++ user-experience/src/main/java/router/client/location/RegExp.java
@@ -7,10 +7,10 @@ import jsinterop.annotations.JsType;
  * This class is simple wrapper around javascript RegExp type.
  * It should be compared or potentially replaced by the equivalent in elemental2.
  */
-@SuppressWarnings( "unused" )
 @JsType( isNative = true, namespace = JsPackage.GLOBAL )
 public class RegExp
 {
+  @SuppressWarnings( "unused" )
   public RegExp( Object pattern )
   {
   }
