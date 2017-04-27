07d49687100edc5fc2cd64df9daf5a5b1be5ff9f
Peter Donald <peter@realityforge.org>
2017-04-27 20:31:09 +1000

    Optimise imports

--- user-experience/src/main/java/router/client/api2/OnEnterCallback.java
+++ user-experience/src/main/java/router/client/api2/OnEnterCallback.java
@@ -3,7 +3,6 @@ package router.client.api2;
 import javax.annotation.Nonnull;
 import jsinterop.annotations.JsFunction;
 import router.client.location.Location;
-import router.client.route.Route;
 
 @JsFunction
 @FunctionalInterface
