322d86b6cbd0f8ebbdec1b8dddf614d7eaaf74f8
Peter Donald <peter@realityforge.org>
2017-05-01 14:25:25 +1000

    Add some further tests around location pattern

--- user-experience/src/test/java/router/client/location/LocationPatternTest.java
+++ user-experience/src/test/java/router/client/location/LocationPatternTest.java
@@ -1,6 +1,8 @@
 package router.client.location;
 
+import java.util.Map;
 import javax.annotation.Nonnull;
+import org.realityforge.guiceyloops.shared.ValueUtil;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
@@ -22,6 +24,41 @@ public class LocationPatternTest
     throws Exception
   {
     assertEquals( LocationPattern.pathToPattern( path ), expected );
+  }
+
+  @Test
+  public void match()
+  {
+    final String location = ValueUtil.randomString();
+    final String[] results = { location, "a", "b", "c" };
+    final String[] parameterKeys = { "param0", null, "param2", "param3" };
+    final LocationPattern pattern = new LocationPattern( new TestRegExp( results ), parameterKeys );
+    final Map<String, Object> matchData = pattern.match( location );
+    assertNotNull( matchData );
+    assertEquals( matchData.size(), 3 );
+    assertEquals( matchData.get( "param0" ), "a" );
+    assertEquals( matchData.get( "p1" ), "b" );
+    assertEquals( matchData.get( "param2" ), "c" );
+  }
+
+  @Test
+  public void match_withNoGroupsInMatch()
+  {
+    final String location = ValueUtil.randomString();
+    final String[] results = { location };
+    final String[] parameterKeys = {};
+    final LocationPattern pattern = new LocationPattern( new TestRegExp( results ), parameterKeys );
+    final Map<String, Object> matchData = pattern.match( location );
+    assertNotNull( matchData );
+    assertEquals( matchData.size(), 0 );
+  }
+
+  @Test
+  public void match_noMatch()
+  {
+    final LocationPattern pattern = new LocationPattern( new TestRegExp(), new String[]{} );
+    final Map<String, Object> matchData = pattern.match( ValueUtil.randomString() );
+    assertNull( matchData );
   }
 
   @Test
