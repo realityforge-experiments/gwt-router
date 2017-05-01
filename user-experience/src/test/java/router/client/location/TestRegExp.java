e5811f70b27c430b9c3653dce4730f72385491dc
Peter Donald <peter@realityforge.org>
2017-05-01 14:23:16 +1000

    Rename the match results so the intent is clearer

--- user-experience/src/test/java/router/client/location/TestRegExp.java
+++ user-experience/src/test/java/router/client/location/TestRegExp.java
@@ -1,23 +1,31 @@
 package router.client.location;
 
+import javax.annotation.Nonnull;
+
 public class TestRegExp
   extends RegExp
 {
-  private String[] _results;
+  private String[] _resultGroups;
+
+  public TestRegExp( @Nonnull final String[] resultGroups )
+  {
+    this();
+    setResultGroups( resultGroups );
+  }
 
   public TestRegExp()
   {
     super( new Object() );
   }
 
-  public void setResults( final String[] results )
+  public void setResultGroups( final String[] resultGroups )
   {
-    _results = results;
+    _resultGroups = resultGroups;
   }
 
   @Override
   public String[] exec( final Object str )
   {
-    return _results;
+    return _resultGroups;
   }
 }
