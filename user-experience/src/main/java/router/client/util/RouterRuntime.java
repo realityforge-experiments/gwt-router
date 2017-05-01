fca31596f32d1cc8bda8c68d8e66fcb64756634f
Peter Donald <peter@realityforge.org>
2017-05-01 14:42:48 +1000

    Add class that can be replaced by GWT to control the checks and guards in place

---
+++ user-experience/src/main/java/router/client/util/RouterRuntime.java
@@ -0,0 +1,13 @@
+package router.client.util;
+
+public final class RouterRuntime
+{
+  private RouterRuntime()
+  {
+  }
+
+  public static boolean enableValidityChecks()
+  {
+    return true;
+  }
+}
